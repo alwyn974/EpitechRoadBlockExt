05d29a4c883b608ca8fc2e381bc7584376ee7cba
Alwyn <alwyn974@gmail.com>
2021-04-20 12:04:27 +0400

    Add project

---
+++ README.md
@@ -0,0 +1,38 @@
+<div align="center">
+    <h1>Web Extension Epitech RoadBlock v1.0</h1>
+
+[![](https://img.shields.io/badge/License-GPL--3.0-brightgreen.svg?style=for-the-badge)](https://github.com/alwyn974/MyEpitechExt/blob/main/LICENSE)
+
+<br><strong> Adds additional information to roadblock modules https://intra.epitech.eu </strong></p>
+
+</div>
+
+## Working On:
+
+- Google Chrome
+- Android
+- Firefox
+
+## How to install
+
+<ul>
+	<li>
+        Chrome
+        <ul>
+            <li><pre>git clone https://github.com/alwyn974/EpitechRoadBlockExt.git</pre></li>
+			<li> Go to <a href="chrome://extensions/">"chrome://extensions/"</a> </li>
+			<li> Activate Developer Mode</li>
+            <li> Load the folder EpitechRoadBlockExt with "Load unpacked"</li>
+		</ul>
+    </li>
+    <li>Firefox
+		<ul>
+            <li><pre>git clone https://github.com/alwyn974/EpitechRoadBlockExt.git</pre></li>
+			<li> Go to <a href="about:debugging#/runtime/this-firefox">"about:debugging#/runtime/this-firefox"</a> </li>
+			<li> Load Temporary Add-on</li>
+            <li> Load the folder EpitechRoadBlockExt</li>
+		</ul>
+	</li>
+</ul>
+
+> :bulb: Don't forget to put a star on the project to support the project
---
+++ roadblock.js
@@ -0,0 +1,132 @@
+const asciiArtLogo = "\n" +
+    " _____      _ _            _      ______                _______ _            _    \n" +
+    "|  ___|    (_| |          | |     | ___ \\              | | ___ | |          | |   \n" +
+    "| |__ _ __  _| |_ ___  ___| |__   | |_/ /___   __ _  __| | |_/ | | ___   ___| | __\n" +
+    "|  __| '_ \\| | __/ _ \\/ __| '_ \\  |    // _ \\ / _` |/ _` | ___ | |/ _ \\ / __| |/ /\n" +
+    "| |__| |_) | | ||  __| (__| | | | | |\\ | (_) | (_| | (_| | |_/ | | (_) | (__|   < \n" +
+    "\\____| .__/|_|\\__\\___|\\___|_| |_| \\_| \\_\\___/ \\__,_|\\__,_\\____/|_|\\___/ \\___|_|\\_\\\n" +
+    "     | |                                                                          \n" +
+    "     |_|                                                                          \n\n";
+const homeUrl = "https://intra.epitech.eu/?format=json";
+const moduleRegex = new RegExp("([A-Z]-[A-Z]{3}-([0-9]|x){3})");
+const cross = "✘";
+const mark = "✔";
+let userJson = {};
+let credits = 0;
+
+const epiLog = (msg, type = "debug") => {
+    switch (type) {
+        case "debug":
+            console.debug("[Epitech RoadBlock] %s", msg);
+            break;
+        case "info":
+            console.info("[Epitech RoadBlock] %s", msg);
+            break;
+        case "error":
+            console.error("[Epitech RoadBlock] %s", msg);
+            break;
+        case "warn":
+            console.warn("[Epitech RoadBlock] %s", msg);
+            break;
+    }
+}
+
+const printLogo = async () => {
+    console.log(asciiArtLogo);
+}
+
+const checkUrl = async () => {
+    epiLog("Checking url to match roadblock modules...")
+    const roadBlockUrlRegex = new RegExp(".*:\\/\\/intra.epitech.eu\\/module\\/[0-9]{4}\\/B-EPI-[0-9]{3}\\/.*");
+    let url = window.location.href;
+    if (!roadBlockUrlRegex.test(url))
+        throw "The url doesn't match Epitech Roadblock modules !";
+}
+
+const getUrl = async (url) => {
+    let data;
+    try {
+        epiLog("Fetching data to " + url);
+        const result = fetch(url, {
+            method: "GET"
+        });
+        data = (await result).json();
+    } catch (e) {
+        epiLog(e, "error");
+        throw "Request error " + url;
+    }
+    return data;
+}
+
+const getJsonModuleInfo = async (module_name) => {
+    let jsons = [];
+    for (const mod of userJson.current) {
+        if (module_name.indexOf("x") >= 0) {
+            let regex_module_name = module_name.replaceAll("x", "[0-9]");
+            let regex = new RegExp(regex_module_name);
+            if (regex.test(mod.code_module))
+                jsons.push(mod);
+        } else if (mod.code_module === module_name) {
+            jsons.push(mod);
+            break;
+        }
+    }
+    return (jsons);
+}
+
+const getModuleInfo = async (line) => {
+    let words = line.replaceAll("(", "").replaceAll(")", "").split(" ");
+    let text = null;
+    for (const word of words) {
+        if (moduleRegex.test(word)) {
+            let json = await getJsonModuleInfo(word);
+            if (json.length !== 0) {
+                for (let i = 0; i < json.length; i++) {
+                    let failed = (json[i].grade === "-" || json[i].grade === "E");
+                    if (text !== null) {
+                        text = text + " | " + "Credits : " + (failed ? "0" : json[i].credits) + "/" + json[i].credits + " " + (failed ? cross : mark)
+                        credits += failed ? 0 : parseInt(json[i].credits);
+                    } else {
+                        text = "Credits : " + (failed ? "0" : json[i].credits) + "/" + json[i].credits + " " + (failed ? cross : mark);
+                        credits += failed ? 0 : parseInt(json[i].credits);
+                    }
+                }
+            } else
+                text = "You are not registered for this module !";
+        }
+    }
+    return text;
+}
+
+const getRoadBLockModules = async () => {
+    let moduleInfo = document.querySelector("div.bloc.main > div.data > div.item.desc > div.text");
+    if (moduleInfo === undefined)
+        throw "Can't get the module info div ! ";
+    let lines = moduleInfo.innerHTML.split("\n");
+    let neededCredits = 0;
+    lines[2].split(" ").forEach(word => {
+        if (!isNaN(parseInt(word)))
+            neededCredits = parseInt(word);
+    })
+    for (let i = 0; i < lines.length; i++) {
+        if (moduleRegex.test(lines[i])) {
+            epiLog(lines[i].trim());
+            let res = await getModuleInfo(lines[i]);
+            lines[i] = lines[i] + " - <strong>" + res + " </strong>";
+        }
+    }
+    epiLog("Needed Credits : " + credits + "/" + neededCredits);
+    let failed = credits < neededCredits;
+    lines[2] = lines[2] + " - <strong> Credits : " + credits + "/" + neededCredits + " " + (failed ? cross : mark) + "</strong>";
+    moduleInfo.innerHTML = lines.join("\n");
+}
+
+const main = async () => {
+    epiLog("Loading Epitech Roadblock extension...");
+    await printLogo();
+    await checkUrl();
+    userJson = await getUrl(homeUrl);
+    await getRoadBLockModules();
+}
+
+main().catch(err => epiLog(err));
